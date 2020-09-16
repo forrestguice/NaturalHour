43de0cbf22008d657489890e0a7a2ab06903fd71
Forrest Guice <forrestguice@users.noreply.github.com>
2020-09-16 01:47:09 -0700

    DisplayStrings

--- app/src/main/java/com/forrestguice/suntimes/romantime/ui/DisplayStrings.java
+++ app/src/main/java/com/forrestguice/suntimes/romantime/ui/DisplayStrings.java
@@ -41,6 +41,25 @@ import java.util.TimeZone;
 
 public class DisplayStrings
 {
+    public static CharSequence romanNumeral(@NonNull Context context, int hour)
+    {
+        switch (hour) {
+            case 1: return "I";
+            case 2: return "II";
+            case 3: return "III";
+            case 4: return "IV";
+            case 5: return "V";
+            case 6: return "VI";
+            case 7: return "VII";
+            case 8: return "VIII";
+            case 9: return "IX";
+            case 10: return "X";
+            case 11: return "XI";
+            case 12: return "XII";
+            default: return "";
+        }
+    }
+
     public static CharSequence formatDate(@NonNull Context context, long date)
     {
         Calendar calendar = Calendar.getInstance();
