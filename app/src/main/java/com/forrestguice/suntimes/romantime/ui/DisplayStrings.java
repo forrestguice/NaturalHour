63dd267a7c40ef5fdb9569f1c988ce14986dd03a
Forrest Guice <forrestguice@users.noreply.github.com>
2020-09-17 21:41:09 -0700

    DisplayStrings

--- app/src/main/java/com/forrestguice/suntimes/romantime/ui/DisplayStrings.java
+++ app/src/main/java/com/forrestguice/suntimes/romantime/ui/DisplayStrings.java
@@ -44,18 +44,11 @@ public class DisplayStrings
     public static CharSequence romanNumeral(@NonNull Context context, int hour)
     {
         switch (hour) {
-            case 1: return "I";
-            case 2: return "II";
-            case 3: return "III";
-            case 4: return "IV";
-            case 5: return "V";
-            case 6: return "VI";
-            case 7: return "VII";
-            case 8: return "VIII";
-            case 9: return "IX";
-            case 10: return "X";
-            case 11: return "XI";
-            case 12: return "XII";
+            case 1: return "I"; case 2: return "II"; case 3: return "III"; case 4: return "IV";
+            case 5: return "V"; case 6: return "VI"; case 7: return "VII"; case 8: return "VIII"; case 9: return "IX";
+            case 10: return "X"; case 11: return "XI"; case 12: return "XII"; case 13: return "XIII"; case 14: return "XIV";
+            case 15: return "XV"; case 16: return "XVI"; case 17: return "XVII"; case 18: return "XVIII"; case 19: return "XIX";
+            case 20: return "XX"; case 21: return "XXI"; case 22: return "XXII"; case 23: return "XXIII"; case 24: return "XXIV";
             default: return "";
         }
     }
