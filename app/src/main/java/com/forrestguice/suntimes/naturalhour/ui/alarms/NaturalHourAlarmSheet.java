f3b81de00252d7a13f721d83f2d276b5cb0d45b4
Forrest Guice <forrestguice@users.noreply.github.com>
2024-10-12 19:36:30 -0700

    BottomSheet
    resource ids

--- app/src/main/java/com/forrestguice/suntimes/naturalhour/ui/alarms/NaturalHourAlarmSheet.java
+++ app/src/main/java/com/forrestguice/suntimes/naturalhour/ui/alarms/NaturalHourAlarmSheet.java
@@ -143,7 +143,7 @@ public class NaturalHourAlarmSheet extends BottomSheetDialogFragment
     {
         if (dialog != null) {
             BottomSheetDialog bottomSheet = (BottomSheetDialog) dialog;
-            FrameLayout layout = (FrameLayout) bottomSheet.findViewById(android.support.design.R.id.design_bottom_sheet);  // for AndroidX, resource is renamed to com.google.android.material.R.id.design_bottom_sheet
+            FrameLayout layout = (FrameLayout) bottomSheet.findViewById(com.google.android.material.R.id.design_bottom_sheet);  // android.support.design.R.id.design_bottom_sheet
             if (layout != null) {
                 BottomSheetBehavior behavior = BottomSheetBehavior.from(layout);
                 behavior.setHideable(false);
