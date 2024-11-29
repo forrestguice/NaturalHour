053e96ccecc4e236867440469b109232c37a4bb0
Forrest Guice <forrestguice@users.noreply.github.com>
2024-11-29 13:09:30 -0700

    menu
    dividers

--- app/src/main/java/com/forrestguice/suntimes/naturalhour/MainActivity.java
+++ app/src/main/java/com/forrestguice/suntimes/naturalhour/MainActivity.java
@@ -26,6 +26,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -509,6 +510,7 @@ public class MainActivity extends AppCompatActivity
         inflater.inflate(R.menu.menu_timezone, popup.getMenu());
         updateTimeZonePopupMenu(popup.getMenu());
         popup.setOnMenuItemClickListener(onTimeZonePopupMenuItemSelected);
+        MenuCompat.setGroupDividerEnabled(popup.getMenu(), true);
         popup.show();
     }
     private void updateTimeZonePopupMenu(Menu menu)
