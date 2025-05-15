2bc08c02368dd8df8922dbbb29aa68a273be96db
Forrest Guice <forrestguice@users.noreply.github.com>
2025-05-15 00:01:43 -0700

    lint

--- app/src/main/java/com/forrestguice/suntimes/naturalhour/ui/AboutDialog.java
+++ app/src/main/java/com/forrestguice/suntimes/naturalhour/ui/AboutDialog.java
@@ -100,7 +100,7 @@ public class AboutDialog extends BottomSheetDialogFragment
             BottomSheetDialog bottomSheet = (BottomSheetDialog) dialog;
             FrameLayout layout = (FrameLayout) bottomSheet.findViewById(com.google.android.material.R.id.design_bottom_sheet);  // android.support.design.R.id.design_bottom_sheet
             if (layout != null) {
-                BottomSheetBehavior behavior = BottomSheetBehavior.from(layout);
+                BottomSheetBehavior<?> behavior = BottomSheetBehavior.from(layout);
                 behavior.setHideable(false);
                 behavior.setSkipCollapsed(false);
                 behavior.setPeekHeight(200);
